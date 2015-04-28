2a30288b0a09a325e5ca0be8160f0ae537615062
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-04-28 18:23:25 -0400

    A classe DictionaryFonteDeDados implementa interface e irá servir para como Mock para os testes automatizados.

---
+++ AnalisadorContabil/AnalisadorContabil/DictionaryFonteDeDados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnalisadorContabil
+{
+    class DictionaryFonteDeDados : IFonteDeDados
+    {
+        IDictionary<String, object> dados = new Dictionary<String, object>();
+
+        public DictionaryFonteDeDados()
+        {
+            dados.Add("C15-0010", "(25 * 3) / 15");
+        }
+
+        public object GetDados(String id)
+        {
+            object valor;
+            dados.TryGetValue(id, out valor);
+
+            return valor;
+        }
+    }
+}
