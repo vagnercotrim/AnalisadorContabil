0aefe8d3950cf44491b1f84c4518e6f8ad5889ee
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-04-27 22:14:12 -0400

    Commit inicial

---
+++ AnalisadorContabil/AnalisadorContabil/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisadorContabil
+{
+    public class Class1
+    {
+    }
+}
