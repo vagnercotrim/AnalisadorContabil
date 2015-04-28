3661367f927906c3c8f980b84a08bb74f0e61be1
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-04-28 17:56:24 -0400

    Outros testes com o componente Formula.

--- AnalisadorContabil/AnalisadorContabil.Testes/FormulaTest.cs
+++ AnalisadorContabil/AnalisadorContabil.Testes/FormulaTest.cs
@@ -16,5 +16,25 @@ namespace AnalisadorContabil.Testes
             Assert.That(valor, Is.EqualTo(75));
         }
 
+        [Test]
+        public void Deve_criar_um_componente_formula_com_operador_condicional_e_retornar_verdadeiro()
+        {
+            IComponente componente = new Formula("C14-006", "25 > 3");
+
+            var valor = componente.GetValor();
+
+            Assert.That(valor, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Deve_criar_um_componente_formula_com_operador_ternario_e_retornar_5()
+        {
+            IComponente componente = new Formula("C14-006", "25 > 3 ? 5 : 6");
+
+            var valor = componente.GetValor();
+
+            Assert.That(valor, Is.EqualTo(5));
+        }
+
     }
 }
