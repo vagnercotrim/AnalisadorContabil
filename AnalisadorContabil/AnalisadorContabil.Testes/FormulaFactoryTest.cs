aef0b23d111c39f36faf271f3705ab345117b551
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-04-28 18:44:15 -0400

    Teste simples que cria um componente do tipo formula através da classe FormulaFactory e buscando a informações da fonte de dados DictionaryFonteDeDados.

---
+++ AnalisadorContabil/AnalisadorContabil.Testes/FormulaFactoryTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace AnalisadorContabil.Testes
+{
+    [TestFixture]
+    public class FormulaFactoryTest
+    {
+
+        [Test]
+        public void Deve_cria_um_componente_formula_atravez_de_formulafacotry()
+        {
+            IDictionary<String, object> dados = new Dictionary<String, object>();
+            dados.Add("C15-0010", "(25 * 3) / 15");
+
+            IFonteDeDados fonteDeDados = new DictionaryFonteDeDados(dados);
+
+            FormulaFactory formulaFactory = new FormulaFactory(fonteDeDados);
+
+            Formula formula = formulaFactory.Criar("C15-0010");
+
+            var valor = formula.GetValor();
+            Assert.That(valor, Is.EqualTo(5));
+        }
+
+    }
+}
