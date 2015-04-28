d81cfbc3a2a6caa90a876c06449ac75b186923f3
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-04-28 18:24:48 -0400

    A classe FormulaFactory ira recuperar as informação de alguma fonte de dados e preencher um componente do tipo Formula.

---
+++ AnalisadorContabil/AnalisadorContabil/FormulaFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AnalisadorContabil
+{
+    public class FormulaFactory
+    {
+        private readonly IFonteDeDados _fonte;
+
+        public FormulaFactory(IFonteDeDados fonte)
+        {
+            _fonte = fonte;
+        }
+
+        public Formula Criar(String id)
+        {
+            object valor = _fonte.GetDados(id);
+
+            return new Formula(id, (String)valor);
+        }
+
+    }
+}
