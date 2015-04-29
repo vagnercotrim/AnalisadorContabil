60d95f585718a62c2e6221d97c7ea1f1406780ac
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-04-28 23:00:07 -0400

    Adicionando atributo Parametros que vai ter todas as informações especificas de cada tipo de componente. Ex: o componente Sql tera os parametros para montar o select sql e retorna a consulta do banco, o componente formula tera a expressão matemática para a ser calculada, e etc.

---
+++ AnalisadorContabil/AnalisadorContabil.Testes/TabelaTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AnalisadorContabil.Testes
+{
+    [TestFixture]
+    public class TabelaTest
+    {
+        [Test]
+        public void Cria_objeto_da_classe_tabela_com_4_parametros()
+        {
+            Tabela tabela1 = new Tabela("C15-0010", "Componente C15-0010", "tabela:gastos;selecione:valor1;campocondicao:conta;valorcondicao:01.02.03.04", 23.23M);
+            
+            IDictionary<String, String> parametros = tabela1.ToDictionary();
+
+            Assert.AreEqual(parametros.Count, 4);
+        }
+    }
+}
--- AnalisadorContabil/AnalisadorContabil/Tabela.cs
+++ AnalisadorContabil/AnalisadorContabil/Tabela.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AnalisadorContabil
 {
@@ -14,5 +16,34 @@ namespace AnalisadorContabil
             Codigo = codigo;
             Valor = valor;
         }
+
+        public Tabela(String codigo, String descricao, String parametros, object valor)
+        {
+            Codigo = codigo;
+            Descricao = descricao;
+            Parametros = parametros;
+            Valor = valor;
+        }
+
+        public IDictionary<String, String> ToDictionary()
+        {
+            IList<String> strings = Parametros.Split(';').ToList();
+            IDictionary<String, String> dictionary = new Dictionary<String, String>();
+
+            foreach (var s in strings)
+            {
+                dictionary.Add(ToKeyValuePair(s));
+            }
+
+            return dictionary;
+        }
+
+        public KeyValuePair<String, String> ToKeyValuePair(String value)
+        {
+            String[] values = value.Split(':');
+
+            return new KeyValuePair<String, String>(values[0], values[1]);
+        }
+
     }
 }
