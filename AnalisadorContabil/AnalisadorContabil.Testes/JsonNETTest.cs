30f3dc651ee2132eef64a01827d983c631a039ba
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-04-30 20:45:03 -0400

    Testtando o Newtonsoft.Json para conhecer como é seu funcionamento.

---
+++ AnalisadorContabil/AnalisadorContabil.Testes/JsonNETTest.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+
+namespace AnalisadorContabil.Testes
+{
+    [TestFixture]
+    public class JsonNetTest
+    {
+        [Test]
+        public void Serializa_um_parametro_com_string()
+        {
+            Parametro parametro = new Parametro("formula", "(2 + 3) * 5");
+
+            String json = JsonConvert.SerializeObject(parametro);
+
+            Assert.AreEqual(json, "{\"Nome\":\"formula\",\"Valor\":\"(2 + 3) * 5\"}");
+        }
+
+        [Test]
+        public void Deserializa_um_parametro_com_string()
+        {
+            String json = "{\"Nome\":\"formula\",\"Valor\":\"(2 + 3) * 5\"}";
+
+            Parametro parametro = JsonConvert.DeserializeObject<Parametro>(json);
+
+            Assert.AreEqual(parametro.Nome, "formula");
+        }
+    }
+}
