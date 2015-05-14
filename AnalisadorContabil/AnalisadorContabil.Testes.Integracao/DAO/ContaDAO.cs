b4ded73e7dd7706c3e407f27232e990d9adc6c17
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-13 23:07:04 -0400

    A classe DAO vai servir pra alimentar o banco com informações a serem consultadas pelo componente.

---
+++ AnalisadorContabil/AnalisadorContabil.Testes.Integracao/DAO/ContaDAO.cs
@@ -0,0 +1,20 @@
+﻿using AnalisadorContabil.Testes.Integracao.Models;
+using NHibernate;
+
+namespace AnalisadorContabil.Testes.Integracao.DAO
+{
+    public class NotificacaoDao
+    {
+        private readonly GenericDao<Conta> _dao;
+
+        public NotificacaoDao(ISession session)
+        {
+            _dao = new GenericDao<Conta>(session);
+        }
+
+        public void Save(Conta conta)
+        {
+            _dao.Save(conta);
+        }
+    }
+}
---
+++ AnalisadorContabil/AnalisadorContabil.Testes.Integracao/DAO/GenericDAO.cs
@@ -0,0 +1,20 @@
+﻿using NHibernate;
+
+namespace AnalisadorContabil.Testes.Integracao.DAO
+{
+    public class GenericDao<T> where T : class
+    {
+
+        private readonly ISession _session;
+
+        public GenericDao(ISession session)
+        {
+            _session = session;
+        }
+
+        public void Save(T t)
+        {
+            _session.Save(t);
+        }
+    }
+}
