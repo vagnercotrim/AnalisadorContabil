09c9c9c4370b5010b27706159ce10d45b5f5d311
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-13 22:42:21 -0400

    Adicionando pacotes do NHIbernate e Sqlite para os testes com o banco de dados.

---
+++ AnalisadorContabil/AnalisadorContabil.Testes.Integracao/InMemoryDatabaseTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AnalisadorContabil.Testes.Integracao
+{
+    public class InMemoryDatabaseTest : IDisposable
+    {
+        private static Configuration _configuration;
+        private static ISessionFactory _sessionFactory;
+        protected ISession Session { get; set; }
+
+        [SetUp]
+        public void Initialize()
+        {
+            _sessionFactory = CreateSessionFactory();
+
+            Session = _sessionFactory.OpenSession();
+
+            SchemaExport export = new SchemaExport(_configuration);
+            export.Execute(true, true, false, Session.Connection, null);
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            Session.Dispose();
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                           .Database(SQLiteConfiguration.Standard.InMemory().ShowSql())
+                           .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ImovelMap>())
+                           .ExposeConfiguration(cfg => _configuration = cfg)
+                           .BuildSessionFactory();
+        }
+    }
+}
