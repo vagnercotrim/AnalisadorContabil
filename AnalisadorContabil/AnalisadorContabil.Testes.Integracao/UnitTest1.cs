1ab6d3967535f0c067ae1236e35b65c7ed64f17c
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-13 22:32:42 -0400

    O novo projeto AnalisadorContabil.Testes.Integracao tem a intenção de separar os testes integração (consulta em banco, arquivo, web services e etc) dos testes unitários.

---
+++ AnalisadorContabil/AnalisadorContabil.Testes.Integracao/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace AnalisadorContabil.Testes.Integracao
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
