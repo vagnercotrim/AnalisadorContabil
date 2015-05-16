ee4af6fafcecddc7b0b32fbcda13cf638a37bf43
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-15 21:34:43 -0400

    Organizando o Dictionary que contóm os componentes a serem testados.

--- AnalisadorContabil/AnalisadorContabil.Testes/Loader/TabelaLoader.cs
+++ AnalisadorContabil/AnalisadorContabil.Testes/Loader/TabelaLoader.cs
@@ -6,42 +6,21 @@ namespace AnalisadorContabil.Testes.Loader
 {
     public class TabelaLoader
     {
-        private IDictionary<String, Tabela> _dados;
+        private readonly IDictionary<String, Tabela> _dados;
 
         public TabelaLoader()
         {
-            _dados = new Dictionary<String, Tabela>();
-
-            Tabela tabela1 = new Tabela("C15N0010", null, "formula", "dictionary", new Parametro("formula", "(25 * 3)"));
-            _dados.Add("C15N0010", tabela1);
-
-            Tabela tabela2 = new Tabela("C15N0011", null, "formula", "dictionary", new Parametro("formula", "[C15N0010] / 15"));
-            _dados.Add("C15N0011", tabela2);
-
-            Tabela tabela3 = new Tabela("C15N0013", null, "formula", "dictionary", new Parametro("formula", "[C15N0011] * 3"));
-            _dados.Add("C15N0013", tabela3);
-
-            Tabela tabela4 = new Tabela("C15N0014", null, "formula", "dictionary", new Parametro("formula", "5 * 5"));
-            _dados.Add("C15N0014", tabela4);
-
-            Tabela tabela5 = new Tabela("C15N0015", null, "formula", "dictionary", new Parametro("formula", "[C15N0010] + [C15N0014]"));
-            _dados.Add("C15N0015", tabela5);
-
-            Tabela tabela6 = new Tabela("C15N0016", null, "formula", "dictionary", new Parametro("formula", "[C15N0010] + [C15N0013]"));
-            _dados.Add("C15N0016", tabela6);
-
-            Tabela tabela7 = new Tabela("C15N0027", null, "sql", "dictionary",
-                new List<Parametro>
-                {
-                    new Parametro("tabela", "tabela"),
-                    new Parametro("campo", "campo"),
-                    new Parametro("condicao", "condicao"),
-                    new Parametro("valor", "'02.01.03'")
-                });
-            _dados.Add("C15N0027", tabela7);
-
-            Tabela tabela8 = new Tabela("C15N0028", null, "formula", "dictionary", new Parametro("formula", "[C15N0027] - 23000"));
-            _dados.Add("C15N0028", tabela8);
+            _dados = new Dictionary<String, Tabela>
+            {
+                { "C15N0010", new Tabela("C15N0010", null, "formula", "dictionary", new Parametro("formula", "(25 * 3)")) },
+                { "C15N0011", new Tabela("C15N0011", null, "formula", "dictionary", new Parametro("formula", "[C15N0010] / 15")) },
+                { "C15N0013", new Tabela("C15N0013", null, "formula", "dictionary", new Parametro("formula", "[C15N0011] * 3")) },
+                { "C15N0014", new Tabela("C15N0014", null, "formula", "dictionary", new Parametro("formula", "5 * 5")) },
+                { "C15N0015", new Tabela("C15N0015", null, "formula", "dictionary", new Parametro("formula", "[C15N0010] + [C15N0014]")) },
+                { "C15N0016", new Tabela("C15N0016", null, "formula", "dictionary", new Parametro("formula", "[C15N0010] + [C15N0013]")) },
+                { "C15N0027", new Tabela("C15N0027", null, "sql",     "dictionary", new List<Parametro> { new Parametro("tabela", "tabela"), new Parametro("campo", "campo"), new Parametro("condicao", "condicao"), new Parametro("valor", "'02.01.03'") }) },
+                { "C15N0028", new Tabela("C15N0028", null, "formula", "dictionary", new Parametro("formula", "[C15N0027] - 23000")) },
+            };
         }
 
         public TabelaDAO CriaTabelaDaoMock()
