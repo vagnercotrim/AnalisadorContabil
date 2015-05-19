e485cbf3f78115e128a4a25a5ab7b4f932d332f8
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-18 21:39:23 -0400

    Criando a fonte de dados RestSharpFonteDeDados para consultar uma api rest e testando sua integração com o componente do tipo Rest .

---
+++ AnalisadorContabil/AnalisadorContabil.Rest/RestSharpFonteDeDados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnalisadorContabil.FonteDeDados;
+
+namespace AnalisadorContabil.Rest
+{
+    public class RestSharpFonteDeDados : IFonteDeDados
+    {
+        private readonly string _endereco;
+
+        public RestSharpFonteDeDados(String endereco)
+        {
+            _endereco = endereco;
+        }
+
+        public object GetDados(string id)
+        {
+            return new DateTime(2015, 5, 15);
+        }
+    }
+}
--- AnalisadorContabil/AnalisadorContabil.Testes/RestTest.cs
+++ AnalisadorContabil/AnalisadorContabil.Testes/RestTest.cs
@@ -1,9 +1,12 @@
-﻿using AnalisadorContabil.Componente;
+﻿using System;
+using AnalisadorContabil.Componente;
 using AnalisadorContabil.Dominio;
 using AnalisadorContabil.Factory;
 using AnalisadorContabil.NHibernate;
+using AnalisadorContabil.Rest;
 using AnalisadorContabil.Testes.Integracao;
 using AnalisadorContabil.Testes.Loader;
+using AnalisadorContabil.Valor;
 using NUnit.Framework;
 
 namespace AnalisadorContabil.Testes
@@ -25,7 +28,7 @@ namespace AnalisadorContabil.Testes
             _consultaSql = new ConsultaSql(Session);
             _factory = new ComponenteFactory(_tabelaDao);
             _factory.AdicionaFonte("sqlite", new NHibernateFonteDeDados(_consultaSql));
-            _factory.AdicionaFonte("api", new NHibernateFonteDeDados(_consultaSql));
+            _factory.AdicionaFonte("api", new RestSharpFonteDeDados("api.teste.com.br"));
 
             _factory.AdicionaVariavelSistema("empresa", 1);
             _factory.AdicionaVariavelSistema("ano", 2015);
@@ -37,7 +40,9 @@ namespace AnalisadorContabil.Testes
         {
             IComponente consulta = _factory.Cria("C15N0101");
 
-            Assert.IsTrue(consulta is Rest);
+            IValor valor = consulta.GetValor();
+
+            Assert.AreEqual(valor.Objeto(), new DateTime(2015, 5, 15));
         }
     }
 }
