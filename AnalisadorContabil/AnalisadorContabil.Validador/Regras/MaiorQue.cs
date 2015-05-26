2d95327d141312c5392ca6f101ce66843ebfc81f
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-25 23:28:27 -0400

    Regra simples de validação para facilitar a implementação da  lógica de validação das informação.

---
+++ AnalisadorContabil/AnalisadorContabil.Validador/Regras/MaiorQue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnalisadorContabil.Validador.Regras
+{
+    public class MaiorQue : IRegraValidacao
+    {
+        private readonly decimal _valor;
+        private readonly decimal _limite;
+        private IList<Notificacao> _notificacoes;
+
+        public MaiorQue(decimal valor, decimal limite)
+        {
+            _valor = valor;
+            _limite = limite;
+            _notificacoes = new List<Notificacao>();
+        }
+
+        public void Validar()
+        {
+            if (_valor > _limite)
+                _notificacoes.Add(new Notificacao(String.Format("O valor {0} não é maior que {1}.", _valor, _limite), Tipo.Alerta));
+        }
+
+        public IList<Notificacao> Notificacoes()
+        {
+            return _notificacoes;
+        }
+    }
+}
