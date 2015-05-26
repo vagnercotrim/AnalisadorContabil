0490803a4a6392ab2842a1c261fbfc543c700578
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-26 00:12:26 -0400

    Outra regra simples para facilitar os testes de integração com a interface IRegraValidacao.

---
+++ AnalisadorContabil/AnalisadorContabil.Validador/Regras/Igual.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnalisadorContabil.Validador.Regras
+{
+    public class Igual : IRegraValidacao
+    {
+        private readonly decimal _valor;
+        private readonly decimal _limite;
+        private readonly IList<Notificacao> _notificacoes;
+
+        public Igual(decimal valor, decimal limite)
+        {
+            _valor = valor;
+            _limite = limite;
+            _notificacoes = new List<Notificacao>();
+        }
+
+        public void Validar()
+        {
+            if (_valor != _limite)
+                _notificacoes.Add(new Notificacao(String.Format("O valor {0} não é igual a {1}.", _valor, _limite), Tipo.Alerta));
+        }
+
+        public IList<Notificacao> Notificacoes()
+        {
+            return _notificacoes;
+        }
+    }
+}
