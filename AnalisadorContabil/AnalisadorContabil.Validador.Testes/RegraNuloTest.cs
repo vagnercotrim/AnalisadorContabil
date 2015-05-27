0e447b6d6f6ddb7219ce9ba39b02a7d5c6803027
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-26 23:38:08 -0400

    A classe RegraNulo para ser retornada para o caso de alguma regra de validação não ser encontrada. Seu objetivo é evitar erros de NullPointerException e ter que verificar sempre a ocorrência de valores null nas variáveis.

---
+++ AnalisadorContabil/AnalisadorContabil.Validador.Testes/RegraNuloTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using AnalisadorContabil.Validador.Regras;
+using NUnit.Framework;
+
+namespace AnalisadorContabil.Validador.Testes
+{
+    [TestFixture]
+    public class RegraNuloTest
+    {
+        [Test]
+        [ExpectedException(typeof (Exception))]
+        public void Verifica_se_a_regranulo_e_nao_retornar_nenhuma_notificacao()
+        {
+            IRegraValidacao regra = new RegraNulo();
+
+            regra.Validar();
+        }
+    }
+}
---
+++ AnalisadorContabil/AnalisadorContabil.Validador/Regras/RegraNulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnalisadorContabil.Validador.Regras
+{
+    public class RegraNulo : IRegraValidacao
+    {
+        public IEnumerable<Notificacao> Validar()
+        {
+            throw new Exception("Regra não encontrada.");
+        }
+    }
+}
