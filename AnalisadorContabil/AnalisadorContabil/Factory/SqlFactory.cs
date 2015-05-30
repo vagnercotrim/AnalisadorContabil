21bca3fca2d85e382a071ac0bd51cffb8483fdf6
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-05-30 11:53:56 -0400

    Excluinfo montagem de sql da classe SqlFactory.

--- AnalisadorContabil/AnalisadorContabil/Factory/SqlFactory.cs
+++ AnalisadorContabil/AnalisadorContabil/Factory/SqlFactory.cs
@@ -11,21 +11,7 @@ namespace AnalisadorContabil.Factory
 
         public static IComponente Cria(Tabela tabela, IDictionary<String, object> variaveisSistema, IFonteDeDados fonte)
         {
-            String sql;
-
-            if (tabela.Get("sql") == null)
-            {
-                String nomecampo = tabela.Get("campo").ToString();
-                String nometabela = tabela.Get("tabela").ToString();
-                String campocondicao = tabela.Get("condicao").ToString();
-                String campovalor = tabela.Get("valor").ToString();
-
-                sql = String.Format("select {0} from {1} where {2} = {3}", nomecampo, nometabela, campocondicao, campovalor);
-            }
-            else
-            {
-                sql = tabela.Get("sql").ToString();
-            }
+            String sql = tabela.Get("sql").ToString();
 
             sql = VariaveisSistema.AtribuiValorVariaveis(sql, variaveisSistema);
 
