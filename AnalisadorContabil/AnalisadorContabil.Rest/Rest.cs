aa8bf67b2527877c9847cebbc59782c99600815b
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-06-02 22:18:10 -0400

    Movendo o metodo Get para a classe Rest.

---
+++ AnalisadorContabil/AnalisadorContabil.Rest/Rest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RestSharp;
+
+namespace AnalisadorContabil.Rest
+{
+    public class Rest
+    {
+        public static IRestRequest Get(String recurso)
+        {
+            IRestRequest request = new RestRequest(recurso, Method.GET);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            return request;
+        }
+    }
+}
--- AnalisadorContabil/AnalisadorContabil.Rest/RestSharpFonteDeDados.cs
+++ AnalisadorContabil/AnalisadorContabil.Rest/RestSharpFonteDeDados.cs
@@ -16,20 +16,11 @@ namespace AnalisadorContabil.Rest
 
         public object GetDados(object recurso)
         {
-            IRestRequest request = Get(recurso.ToString());
+            IRestRequest request = Rest.Get(recurso.ToString());
 
             IRestResponse response = _client.Execute(request);
 
-            object resultado = JsonConvert.DeserializeObject(response.Content);
-
-            return resultado;
-        }
-        
-        public static IRestRequest Get(String recurso)
-        {
-            IRestRequest request = new RestRequest(recurso, Method.GET);
-            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
-            return request;
+            return JsonConvert.DeserializeObject(response.Content);
         }
     }
 }
